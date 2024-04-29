4a6d88475793c635d8584796308dbdc9fc6abcb3
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-04-29 19:15:23 +0300

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import "../App.css";
+import React from "react";
+import AccountContainer from "./AccountContainer";
 
-function App(){
-    const title = "The Royal Bank of Flatiron";
-
-
-    return (<div className="App">
-    <div className="content">
-    <h1>{title}</h1>
+function App() {
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment violet inverted">
+        <h2>The Royal Bank of Flatiron</h2>
+      </div>
+      <AccountContainer />
     </div>
-    </div>
-    );
-};
+  );
+}
 
 export default App;
